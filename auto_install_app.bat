86e9d55586ee4c088c9eadd1e903c2f595dd1a13
HexorCatZ <31932384+HexorCatZ@users.noreply.github.com>
2024-05-05 15:00:59 +0800

    Update auto_install_app.bat
    add additional software list. add new category.

--- auto_install_app.bat
+++ auto_install_app.bat
@@ -12,6 +12,24 @@ winget -v >nul 2>nul && ( break ) || (
     echo.
     echo Please update App Installer from Microsoft Store.
     echo Press any key to exit . . .
+    choice /C yn /N /M "Force update using chocolatey? [Y/N] : "
+    set choice=%errorlevel%
+    if %choice%==1 (
+        set m3o1=+%m3o1:~1% & :: Google.Chrome
+        set m7o1=+%m7o1:~1% & :: VideoLAN.VLC
+        set m8o2=+%m8o2:~1% & :: Adobe.Acrobat.Reader.32-bit
+        set m8o5=+%m8o5:~1% & :: RARLab.WinRAR
+        goto list )
+    if %choice%==2 (
+        set m3o1=+%m3o1:~1% & :: Google.Chrome
+        set m7o1=+%m7o1:~1% & :: VideoLAN.VLC
+        set m8o2=+%m8o2:~1% & :: Adobe.Acrobat.Reader.32-bit
+        set m8o5=+%m8o5:~1% & :: RARLab.WinRAR
+        set m4o1=+%m4o1:~1% & :: Microsoft.Office
+        set m6o6=+%m6o6:~1% & :: Zoom.Zoom
+        set m6o2=+%m6o2:~1% & :: Microsoft.Teams
+        set m5o6=+%m5o6:~1% & :: Microsoft.Skype
+        goto list )
     pause 2>nul >nul
     exit /b %errorlevel%
 )
@@ -81,71 +99,82 @@ set m3o7=-
 set m3o8=-
 set m3o9=-
 rem ===== menu4 =====
-set menu4=Online Chatting Application
-set m4o1=-Alibaba.DingTalk                  DingTalk
-set m4o2=-Discord.Discord                   Discord
-set m4o3=-Microsoft.Skype                   Skype
-set m4o4=-Telegram.TelegramDesktop          Telegram Desktop
-set m4o5=-XPFCKBRNFZQ62G                    WeChat                        [MS Store]
-set m4o6=-9NKSQGP7F2NH                      WhatsApp                      [MS Store]
-set m4o7=-Facebook.Messenger                Facebook Messenger
-set m4o8=-9NBLGGH5L9XT                      Instagram
+set menu4=Office Application
+set m4o1=-Microsoft.Office                  Microsoft Office 365
+set m4o2=-Kingsoft.WPSOffice                WPS Office
+set m4o3=-Kingsoft.WPSOffice.CN             WPS Office Chinese
+set m4o4=-TheDocumentFoundation.LibreOffice LibreOffice
+set m4o5=-Apache.OpenOffice                 OpenOffice
+set m4o6=-
+set m4o7=-
+set m4o8=-
 set m4o9=-
 rem ===== menu5 =====
-set menu5=Online Meeting and Remote Desktop Application
-set m5o1=-AnyDeskSoftwareGmbH.AnyDesk       AnyDesk
-set m5o2=-Microsoft.Teams                   Microsoft Teams
-set m5o3=-Parsec.Parsec                     Parsec
-set m5o4=-TeamViewer.TeamViewer             TeamViewer
-set m5o5=-GlavSoft.TightVNC                 TightVNC
-set m5o6=-Zoom.Zoom                         Zoom
-set m5o7=-
-set m5o8=-
+set menu5=Online Chatting Application
+set m5o1=-Alibaba.DingTalk                  DingTalk
+set m5o2=-Discord.Discord                   Discord
+set m5o3=-Microsoft.Skype                   Skype
+set m5o4=-Telegram.TelegramDesktop          Telegram Desktop
+set m5o5=-XPFCKBRNFZQ62G                    WeChat                        [MS Store]
+set m5o6=-9NKSQGP7F2NH                      WhatsApp                      [MS Store]
+set m5o7=-Facebook.Messenger                Facebook Messenger
+set m5o8=-9NBLGGH5L9XT                      Instagram
 set m5o9=-
 rem ===== menu6 =====
-set menu6=Multimedia Software
-set m6o1=-VideoLAN.VLC                      VLC media player
-set m6o2=-GOMLab.GOMPlayer                  GOM Player
-set m6o3=-Winamp.Winamp                     Winamp
-set m6o4=-CodecGuide.K-LiteCodecPack.Basic  K-Lite Codec Pack Basic
-set m6o5=-9NCBCSZSJRSB                      Spotify - Music and Podcasts  [MS Store]
-set m6o6=-Ytmdesktop.Ytmdesktop             YouTube Music Desktop App
-set m6o7=-Audacity.Audacity                 Audacity
-set m6o8=-MiXXX.MiXXX                       mixxx
+set menu6=Online Meeting and Remote Desktop Application
+set m6o1=-AnyDeskSoftwareGmbH.AnyDesk       AnyDesk
+set m6o2=-Microsoft.Teams                   Microsoft Teams
+set m6o3=-Parsec.Parsec                     Parsec
+set m6o4=-TeamViewer.TeamViewer             TeamViewer
+set m6o5=-GlavSoft.TightVNC                 TightVNC
+set m6o6=-Zoom.Zoom                         Zoom
+set m6o7=-
+set m6o8=-
 set m6o9=-
 rem ===== menu7 =====
-set menu7=Utilities Software
-set m7o1=-7zip.7zip                         7-Zip
-set m7o2=-Adobe.Acrobat.Reader.32-bit       Adobe Acrobat Reader
-set m7o3=-Notepad++.Notepad++               Notepad++
-set m7o4=-CodeSector.TeraCopy               TeraCopy
-set m7o5=-RARLab.WinRAR                     WinRAR
-set m7o6=-qBittorrent.qBittorrent           qBittorrent
-set m7o7=-FastCopy.FastCopy                 FastCopy
-set m7o8=-
+set menu7=Multimedia Software
+set m7o1=-VideoLAN.VLC                      VLC media player
+set m7o2=-GOMLab.GOMPlayer                  GOM Player
+set m7o3=-Winamp.Winamp                     Winamp
+set m7o4=-CodecGuide.K-LiteCodecPack.Basic  K-Lite Codec Pack Basic
+set m7o5=-9NCBCSZSJRSB                      Spotify - Music and Podcasts  [MS Store]
+set m7o6=-Ytmdesktop.Ytmdesktop             YouTube Music Desktop App
+set m7o7=-Audacity.Audacity                 Audacity
+set m7o8=-MiXXX.MiXXX                       mixxx
 set m7o9=-
 rem ===== menu8 =====
-set menu8=Game Related Application
-set m8o1=-Valve.Steam                       Steam
-set m8o2=-EpicGames.EpicGamesLauncher       Epic Games Launcher
-set m8o3=-ElectronicArts.EADesktop          EA app
-set m8o4=-
-set m8o5=-
-set m8o6=-
-set m8o7=-
+set menu8=Utilities Software
+set m8o1=-7zip.7zip                         7-Zip
+set m8o2=-Adobe.Acrobat.Reader.32-bit       Adobe Acrobat Reader
+set m8o3=-Notepad++.Notepad++               Notepad++
+set m8o4=-CodeSector.TeraCopy               TeraCopy
+set m8o5=-RARLab.WinRAR                     WinRAR
+set m8o6=-qBittorrent.qBittorrent           qBittorrent
+set m8o7=-FastCopy.FastCopy                 FastCopy
 set m8o8=-
 set m8o9=-
 rem ===== menu9 =====
-set menu9=System Monitor Application
-set m9o1=-CPUID.CPU-Z                       CPUID CPU-Z
-set m9o2=-CPUID.HWMonitor                   CPUID HWMonitor
-set m9o3=-REALiX.HWiNFO                     HWiNFO
-set m9o4=-Guru3D.Afterburner                Afterburner
-set m9o5=-FinalWire.AIDA64.Extreme          AIDA64 Extreme
-set m9o6=-Unigine.SuperpositionBenchmark    Unigine Superposition Benchmark
-set m9o7=-Unigine.HeavenBenchmark           Unigine Heaven Benchmark
+set menu9=Game Related Application
+set m9o1=-Valve.Steam                       Steam
+set m9o2=-EpicGames.EpicGamesLauncher       Epic Games Launcher
+set m9o3=-ElectronicArts.EADesktop          EA app
+set m9o4=-
+set m9o5=-
+set m9o6=-
+set m9o7=-
 set m9o8=-
 set m9o9=-
+rem ===== menu10 =====
+set menu10=System Monitor Application
+set m10o1=-CPUID.CPU-Z                       CPUID CPU-Z
+set m10o2=-CPUID.HWMonitor                   CPUID HWMonitor
+set m10o3=-REALiX.HWiNFO                     HWiNFO
+set m10o4=-Guru3D.Afterburner                Afterburner
+set m10o5=-FinalWire.AIDA64.Extreme          AIDA64 Extreme
+set m10o6=-Unigine.SuperpositionBenchmark    Unigine Superposition Benchmark
+set m10o7=-Unigine.HeavenBenchmark           Unigine Heaven Benchmark
+set m10o8=-
+set m10o9=-
 goto mainmenu
 
 :autoconfig
@@ -188,38 +217,39 @@ if %choice%==10 goto start
 if %choice%==11 goto :EOF
 if %choice%==1 (
     set m3o1=+%m3o1:~1% & :: Google.Chrome
-    set m6o1=+%m6o1:~1% & :: VideoLAN.VLC
-    set m7o2=+%m7o2:~1% & :: Adobe.Acrobat.Reader.32-bit
-    set m7o5=+%m7o5:~1% & :: RARLab.WinRAR
+    set m7o1=+%m7o1:~1% & :: VideoLAN.VLC
+    set m8o2=+%m8o2:~1% & :: Adobe.Acrobat.Reader.32-bit
+    set m8o5=+%m8o5:~1% & :: RARLab.WinRAR
     goto list )
 if %choice%==2 (
     set m3o1=+%m3o1:~1% & :: Google.Chrome
-    set m6o1=+%m6o1:~1% & :: VideoLAN.VLC
-    set m7o2=+%m7o2:~1% & :: Adobe.Acrobat.Reader.32-bit
-    set m7o5=+%m7o5:~1% & :: RARLab.WinRAR
-    set m5o6=+%m5o6:~1% & :: Zoom.Zoom
-    set m5o2=+%m5o2:~1% & :: Microsoft.Teams
-    set m4o6=+%m4o6:~1% & :: Microsoft.Skype
+    set m7o1=+%m7o1:~1% & :: VideoLAN.VLC
+    set m8o2=+%m8o2:~1% & :: Adobe.Acrobat.Reader.32-bit
+    set m8o5=+%m8o5:~1% & :: RARLab.WinRAR
+    set m4o1=+%m4o1:~1% & :: Microsoft.Office
+    set m6o6=+%m6o6:~1% & :: Zoom.Zoom
+    set m6o2=+%m6o2:~1% & :: Microsoft.Teams
+    set m5o6=+%m5o6:~1% & :: Microsoft.Skype
     goto list )
 if %choice%==3 (
     set m3o1=+%m3o1:~1% & :: Google.Chrome
-    set m6o1=+%m6o1:~1% & :: VideoLAN.VLC
-    set m7o2=+%m7o2:~1% & :: Adobe.Acrobat.Reader.32-bit
-    set m7o5=+%m7o5:~1% & :: RARLab.WinRAR
+    set m7o1=+%m7o1:~1% & :: VideoLAN.VLC
+    rem set m8o2=+%m8o2:~1% & :: Adobe.Acrobat.Reader.32-bit
+    set m8o5=+%m8o5:~1% & :: RARLab.WinRAR
     set m1o1=+%m1o1:~1% & :: vcredist
     set m1o2=+%m1o2:~1% & :: Nvidia.GeForceExperience
     set m3o4=+%m3o4:~1% & :: Opera.OperaGX
-    set m4o2=+%m4o2:~1% & :: Discord.Discord
-    set m8o1=+%m8o1:~1% & :: Valve.Steam
-    set m8o2=+%m8o2:~1% & :: EpicGames.EpicGamesLauncher
-    set m8o3=+%m8o3:~1% & :: ElectronicArts.EADesktop
+    set m5o2=+%m5o2:~1% & :: Discord.Discord
+    set m9o1=+%m9o1:~1% & :: Valve.Steam
+    set m9o2=+%m9o2:~1% & :: EpicGames.EpicGamesLauncher
+    set m9o3=+%m9o3:~1% & :: ElectronicArts.EADesktop
     goto list )
 if %choice%==9 goto autoconfig
 goto mainmenu
 
 :start
 set menu=1
-set menumax=9
+set menumax=10
 :chooseapps
 cls
 if %menu% leq 0 goto mainmenu
@@ -308,7 +338,7 @@ echo y | winget upgrade
 cls
 winget list > "%temp%\WinGet\list.txt"
 
-for /l %%a in (1,1,9) do (
+for /l %%a in (1,1,%menumax%) do (
     for /l %%b in (1,1,9) do (
         for /f "tokens=2,* delims==" %%i in ('set m%%ao%%b') do (
             set install=%%i
